2621d4478d8371b1a3114b9eaaa2a769d5abb8e7
HyeongwooIM <hyeongwoo1279@gmail.com>
2022-10-12 20:57:43 +0900

    map 추가

--- 02_so_long/check_map.c
+++ 02_so_long/check_map.c
@@ -1,31 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: him <him@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/10 18:12:12 by him               #+#    #+#             */
-/*   Updated: 2022/10/12 20:32:17 by him              ###   ########.fr       */
+/*   Created: 2022/10/12 20:46:30 by him               #+#    #+#             */
+/*   Updated: 2022/10/12 20:50:01 by him              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-# include <unistd.h>
-# include "gnl/get_next_line.h"
-
-typedef struct s_map
+int	check_line_len(t_param *param)
 {
-	char	*map_line;
-	t_map	*next;
-}t_map;
-
-typedef struct s_param
-{
-	int		fd;
 	t_map	*map;
-}t_param;
+	int		len;
 
-#endif
+	len = 0;
+	map = param->map;
+	while (map)
+	{
+		map = map->next;
+		len++;
+	}
+	return (len);
+}
+
+void	check_map(t_param *param)
+{
+	int	line_num;
+
+	line_num = check_line_len(param);
+}
--- 02_so_long/map.c
+++ 02_so_long/map.c
@@ -6,15 +6,50 @@
 /*   By: him <him@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 19:25:13 by him               #+#    #+#             */
-/*   Updated: 2022/10/12 20:15:15 by him              ###   ########.fr       */
+/*   Updated: 2022/10/12 20:46:11 by him              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	free_map(t_param *param)
+{
+	t_map	*temp;
+	t_map	*map;
+
+	map = param->map;
+	while (map)
+	{
+		temp = map->next;
+		free(map->map_line);
+		free(map);
+		map = temp;
+	}
+	free(map);
+}
+
 void	copyline(char *str, t_param *param)
 {
-	
+	t_map	*line;
+	t_map	*return_map;
+
+	return_map = param->map;
+	line = malloc(sizeof(t_map));
+	if (!line)
+	{
+		all_free(param);
+		return ;
+	}
+	line->next = 0;
+	line->map_line = ft_strdup(str);
+	if (!param->map)
+		param->map = line;
+	else
+	{
+		while (return_map->next)
+			return_map = return_map->next;
+		return_map->next = line;
+	}
 }
 
 int	get_map(t_param *param)
