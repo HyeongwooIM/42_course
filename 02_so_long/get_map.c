77c7f668e73508548d9823967b45046b74c9daae
HyeongwooIM <hyeongwoo1279@gmail.com>
2022-10-13 16:42:09 +0900

    처음부터 시작

---
+++ 02_so_long/get_map.c
@@ -0,0 +1,6 @@
+void	get_map(t_param *param)
+{
+	char	*str;
+
+	str = get_next_line(param->fd);
+}
--- 02_so_long/so_long.h
+++ 02_so_long/so_long.h
@@ -1,31 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: him <him@student.42seoul.kr>               +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/10 18:12:12 by him               #+#    #+#             */
-/*   Updated: 2022/10/12 20:32:17 by him              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <unistd.h>
-# include "gnl/get_next_line.h"
-
-typedef struct s_map
+typedef struct	s_map
 {
-	char	*map_line;
-	t_map	*next;
+	char			*line;
+	struct s_map	*next;
 }t_map;
 
-typedef struct s_param
+typedef struct	s_param
 {
-	int		fd;
 	t_map	*map;
+	int		fd;
 }t_param;
 
-#endif
+
