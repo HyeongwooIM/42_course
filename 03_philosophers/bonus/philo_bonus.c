278647ac1d7d2301fab5f7949001e416849615e2
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-03-15 11:21:54 +0900

    보너스 및 만다토리

--- 03_philosophers/bonus/philo_bonus.c
+++ 03_philosophers/bonus/philo_bonus.c
@@ -1,5 +1,116 @@
 #include "philo_bonus.h"
 
+void	ft_error(char *message)
+{
+	printf("[Error] %s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+long long	init_time(void)
+{
+	struct timeval	tp;
+
+	if (gettimeofday(&tp, NULL) < 0)
+		return (printf("[Error] Can't get time\n"));
+	return ((tp.tv_sec * 1000) + (tp.tv_usec / 1000));
+}
+
+static void	free_process(t_info *info, t_philo *philo)
+{
+	sem_close(info->fork);
+	sem_close(info->print);
+	sem_close(info->check_last_eat);
+	sem_unlink("fork");
+	sem_unlink("print");
+	sem_unlink("check_last_eat");
+	free(philo);
+	free(info->pid);
+}
+
+int	ft_atoi(const char *s)
+{
+	long long	result;
+	int			sign;
+
+	result = 0;
+	sign = 1;
+	while (((*s >= 9 && *s <= 13) || *s == ' ') && *s)
+		s++;
+	if ((*s == '+' || *s == '-') && *s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+	while (*s)
+	{
+		if (!('0' <= *s && *s <= '9'))
+			return (-1);
+		result = result * 10 + (*s - '0');
+		if ((sign > 0 && result * sign > INT_MAX) || \
+			(sign < 0 && result * sign < INT_MIN))
+			return (-1);
+		s++;
+	}
+	return (result * sign);
+}
+
+void	wait_time(long long time)
+{
+	long long	start_time;
+
+	start_time = init_time();
+	while ((init_time() - start_time < time))
+		usleep(250);
+}
+
+void	print_philo(t_info *info, int id, int status)
+{
+	long long	now_time;
+
+	now_time = init_time() - info->start_time;
+	sem_wait(info->print);
+	if (status == FORK)
+		printf("%lld %d has taken a fork\n", now_time, id);
+	else if (status == EAT)
+		printf("%lld %d is eating\n", now_time, id);
+	else if (status == SLEEP)
+		printf("%lld %d is sleeping\n", now_time, id);
+	else if (status == THINK)
+		printf("%lld %d is thinking\n", now_time, id);
+	else if (status == DIE)
+	{
+		printf("%lld %d died\n", now_time, id);
+		return ;
+	}
+	sem_post(info->print);
+}
+
+void	kill_pids(t_info *info, int id)
+{
+	int	i;
+
+	i = -1;
+	while (++i < id)
+		kill(info->pid[i], SIGINT);
+}
+
+static void	eat_philo(t_info *info, t_philo *philo)
+{
+	sem_wait(info->fork);
+	print_philo(info, philo->id, FORK);
+	sem_wait(info->fork);
+	print_philo(info, philo->id, FORK);
+	print_philo(info, philo->id, EAT);
+	philo->num_eat += 1;
+	sem_wait(info->check_last_eat);
+	philo->time_last_eat = init_time();
+	sem_post(info->check_last_eat);
+	wait_time(info->eat_time);
+	sem_post(info->fork);
+	sem_post(info->fork);
+}
+
 void	*monitoring(void	*ph)
 {
 	t_philo	*philo;
@@ -61,6 +172,27 @@ int	init_process(t_info *info, t_philo *philo)
 			run_philo(info, &philo[i]);
 	}
 	return (0);
+}
+void	wait_process(t_info *info)
+{
+	int	i;
+	int	status;
+
+	i = -1;
+	while (++i < info->philo_num)
+	{
+		waitpid(-1, &status, 0);
+		if (status != 0)
+		{
+			kill_pids(info, info->philo_num);
+			usleep(2000);
+			sem_post(info->print);
+			sem_post(info->check_last_eat);
+			break ;
+		}
+		sem_post(info->print);
+		sem_post(info->check_last_eat);
+	}
 }
 
 int	init_info(t_info *info)
