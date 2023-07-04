b5d9e293267f603b314d2d4a150805b39e861cd2
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-07-04 18:56:12 +0900

    cpp01

---
+++ 04_cpp/cpp_module01/ex06/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+int main(int ac, char *av[])
+{
+	Harl harl;
+
+	if (ac != 2)
+	{
+		std::cout << "argv error" << std::endl;
+		return (1);
+	}
+	harl.complain(av[1]);
+
+	return 0;
+}
